d4b72ee9d9a3db3681d01f0a0f359c1dec87ba8b
EC2 Default User <ec2-user@ip-172-31-41-217.us-east-2.compute.internal>
2023-03-08 05:47:12 +0000

    Create log_cleanup.sh

---
+++ log_cleanup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#==========================================================================================================#
+#   Author: Sugumar Srinivasan                                                                             #
+#   NTID: ssrke                                                                                            #
+#   Creation Date: 03/08/2022                                                                              #
+#   Modified Date: 03/16/2022                                                                              #
+#   Description: This shell script will clean_up the older files based on the user input(FILE_PATH, USER,  #
+#                GROUP, DURATION).                                                                         #
+#                                                                                                          #
+#   Syntax:  sh log_cleanup.sh <LOG_PATH> <USER> <GROUP> <DURATION>                                        #
+#   Example: sh log_cleanup.sh /allstate/log/ hadoop hadoop 90                                             #
+#==========================================================================================================#
+
+LOG_PATH=$1
+USER=$2
+GROUP=$3
+DURATION=$4
+
+#find $LOG_PATH -type f -user $USER -group $GROUP -mtime +$DURATION -exec ls {} \; | wc -l
+find $LOG_PATH -type f -user $USER -group $GROUP -mtime +$DURATION -exec rm -rf {} \;
